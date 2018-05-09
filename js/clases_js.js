49c9a210db88611f4a58739a972cf1b6c4c5e3db
Jisargu <jonathnsaa@gmail.com>
2018-05-09 14:13:14 -0500

    Arreglos dinamicos de objetos

--- js/clases_js.js
+++ js/clases_js.js
@@ -6,13 +6,13 @@ function restaurante() {
             this.domicilio = domicilio;
             this.telefono = telefono;
         }
-        get nombre(){
+        get nombre() {
             return this._nombre;
         }
-        get domicilio(){
+        get domicilio() {
             return this._domicilio;
         }
-        get telefono(){
+        get telefono() {
             return this._telefono;
         }
         set nombre(nombre) {
@@ -29,14 +29,31 @@ function restaurante() {
         }
 
     }//fin clase Restaurante
-    let nuevoRestaurante= new Restaurante();
-    let nombre = prompt("Escribe el nombre del restaurante");
-    let domicilio = prompt("Escribe el domicilio del restaurante");
-    let telefono = prompt("Escribe el telefono del restaurante");
-    nuevoRestaurante.nombre=nombre;
-    nuevoRestaurante.domicilio=domicilio;
-    nuevoRestaurante.telefono=telefono;
-    nuevoRestaurante.mostrarDatos();
-    
+    /*    let nuevoRestaurante = new Restaurante();
+       let nombre = prompt("Escribe el nombre del restaurante");
+       let domicilio = prompt("Escribe el domicilio del restaurante");
+       let telefono = prompt("Escribe el telefono del restaurante");
+       nuevoRestaurante.nombre = nombre;
+       nuevoRestaurante.domicilio = domicilio;
+       nuevoRestaurante.telefono = telefono;
+       nuevoRestaurante.mostrarDatos(); */
+
+
+
+    let cantidad_restaurantes = prompt("Escribe la cantidad de restaurantes: ");
+    var arreglo = [];
+    let i;
+    for (i = 0; i < cantidad_restaurantes; i++) {
+        arreglo.push(new Restaurante());
+        let nombre = prompt("Escribe el nombre del restaurante: " + (i + 1));
+        let domicilio = prompt("Escribe el domicilio del restaurante" + (i + 1));
+        let telefono = prompt("Escribe el telefono del restaurante" + (i + 1));
+        arreglo[i].nombre = nombre;
+        arreglo[i].domicilio = domicilio;
+        arreglo[i].telefono = telefono;
+    }
+    for (i = 0; i < cantidad_restaurantes; i++) {
+        arreglo[i].mostrarDatos();
+    }
 
 }
