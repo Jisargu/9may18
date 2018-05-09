8f3bf32c3018934883baec9e46a10ed329e55c4d
Jisargu <jonathnsaa@gmail.com>
2018-05-09 14:57:55 -0500

    Sacar la clase de la funcion y colocar PRIMERO set y despues get

--- js/clases_js.js
+++ js/clases_js.js
@@ -1,45 +1,50 @@
 "use strict";
+
+class Restaurante {
+    constructor(nombre, domicilio, telefono) {
+        //Invoca el set porque primero set y luego get
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+    
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+    set domicilio(domicilio) {
+        this._domicilio = domicilio;
+    }
+    set telefono(telefono) {
+        this._telefono = telefono;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+    get domicilio() {
+        return this._domicilio;
+    }
+    get telefono() {
+        return this._telefono;
+    }
+    mostrarDatos() {
+        alert(this.nombre + '\n' + this.domicilio + '\n' + this.telefono);
+    }
+
+}//fin clase Restaurante
+
+//comentarios para 1 sola clase
+/*    let nuevoRestaurante = new Restaurante();
+   let nombre = prompt("Escribe el nombre del restaurante");
+   let domicilio = prompt("Escribe el domicilio del restaurante");
+   let telefono = prompt("Escribe el telefono del restaurante");
+   nuevoRestaurante.nombre = nombre;
+   nuevoRestaurante.domicilio = domicilio;
+   nuevoRestaurante.telefono = telefono;
+   nuevoRestaurante.mostrarDatos(); */
+
+
 function restaurante() {
-    class Restaurante {
-        constructor(nombre, domicilio, telefono) {
-            this.nombre = nombre;
-            this.domicilio = domicilio;
-            this.telefono = telefono;
-        }
-        get nombre() {
-            return this._nombre;
-        }
-        get domicilio() {
-            return this._domicilio;
-        }
-        get telefono() {
-            return this._telefono;
-        }
-        set nombre(nombre) {
-            this._nombre = nombre;
-        }
-        set domicilio(domicilio) {
-            this._domicilio = domicilio;
-        }
-        set telefono(telefono) {
-            this._telefono = telefono;
-        }
-        mostrarDatos() {
-            alert(this.nombre + '\n' + this.domicilio + '\n' + this.telefono);
-        }
-
-    }//fin clase Restaurante
-    /*    let nuevoRestaurante = new Restaurante();
-       let nombre = prompt("Escribe el nombre del restaurante");
-       let domicilio = prompt("Escribe el domicilio del restaurante");
-       let telefono = prompt("Escribe el telefono del restaurante");
-       nuevoRestaurante.nombre = nombre;
-       nuevoRestaurante.domicilio = domicilio;
-       nuevoRestaurante.telefono = telefono;
-       nuevoRestaurante.mostrarDatos(); */
-
-
-
     let cantidad_restaurantes = prompt("Escribe la cantidad de restaurantes: ");
     var arreglo = [];
     let i;
